5012365edec8ad1a42ebcabca83d55fd60f18b81
MIAKASH19 <121384260+MIAKASH19@users.noreply.github.com>
2024-02-22 03:06:10 +0600

    second commit

--- src/components/SendMail.jsx
+++ src/components/SendMail.jsx
@@ -3,9 +3,33 @@ import "./SendMail.css";
 import { IconButton, Button } from "@mui/material";
 import { IoClose } from "react-icons/io5";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { ErrorMessage } from "@hookform/error-message";
+
+const validationSchema = yup.object().shape({
+
+  subject: yup.string().required("Subject is required"),
+
+  message: yup.string().required("Write Message first"),
+
+  to: yup.string().email("email is invalid").required("Email is required"),
+});
 
 const SendMail = () => {
-  const { register, watch, handleSubmit, errors } = useForm();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data, null, 2));
+  };
+
   return (
     <div className="sendMail">
       <div className="sendMail-header">
@@ -14,34 +38,55 @@ const SendMail = () => {
           <IoClose className="sendMail-close" />
         </IconButton>
       </div>
-      <form>
-        <input
-          name="to"
-          type="text"
-          placeholder="Recipents"
-          {...register("to", { required: true })}
-        />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input
+            name="to"
+            type="email"
+            placeholder="To"
+            {...register("to")}
+          />
 
-        <input
-          name="subject"
-          type="text"
-          placeholder="Subject"
-          {...register("to", { required: true })}
-        />
+          <ErrorMessage
+            errors={errors}
+            name="to"
+            render={({ message }) => <p className="sendMail-errors">{message}</p>}
+          />
+        </div>
 
-        <input
-          name="message"
-          type="text"
-          placeholder="Write Message..."
-          className="sendMail-Message"
-          {...register("to", { required: true })}
-        />
+        <div>
+          <input
+            name="subject"
+            type="text"
+            placeholder="Subject"
+            {...register("subject")}
+          />
+
+          <ErrorMessage
+            errors={errors}
+            name="subject"
+            render={({ message }) => <p className="sendMail-errors">{message}</p>}
+          />
+        </div>
+
+        <div className="sendMail-Message">
+          <input
+            name="message"
+            type="text"
+            placeholder="Write Message..."
+            {...register("message")}
+          />
+
+          <ErrorMessage
+            errors={errors}
+            name="message"
+            render={({ message }) => <p className="sendMail-errors">{message}</p>}
+          />
+        </div>
 
         <div className="sendMail-options">
           <Button
             className="sendMail-Button"
-            color="primary"
-            variant="contained"
             type="submit"
           >
             Send
